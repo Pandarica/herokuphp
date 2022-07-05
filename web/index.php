3260f656941e13bfeb60144207814aeb1c08a536
Pandarica <20022226@myrp.edu.sg>
2022-07-06 00:10:09 +0800

    commit message goes here

--- web/index.php
+++ web/index.php
@@ -1,3 +1,12 @@
-<?
+<html lang="en">
+    <head>
+        <meta
+            name="Test"
+            content="Testing website">
+        <title>Testing website</title>
+    </head>
+    <body>
+        Hello World!
+    </body>
+</html>
 
-echo "Hello World!"?>
